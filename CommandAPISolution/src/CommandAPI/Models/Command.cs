aa3857ba371730d775630228731c10f4fe83811e
fawad irfan sheikh <fawad313@acm.org>
2023-11-28 00:27:03 +0000

    Added Command model to API project

--- CommandAPISolution/src/CommandAPI/Models/Command.cs
+++ CommandAPISolution/src/CommandAPI/Models/Command.cs
@@ -1,9 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace CommandAPI.Models;
 
 public class Command 
 {
+    [Key]
+    [Required]
     public int Id { get; set; }
-    public string HowTo { get; set; }
-    public string Line { get; set; }
-    public string Platform { get; set; }
+
+    [Required]
+    [MaxLength(250)]
+    public string HowTo { get; set; } = string.Empty;
+
+    [Required]
+    public string Line { get; set; } = string.Empty;
+    
+    [Required]
+    public string Platform { get; set; } = string.Empty;
 }
