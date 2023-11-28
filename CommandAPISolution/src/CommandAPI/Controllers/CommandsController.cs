e7ac0f2f90adaa8ed424214359487b192f1ee670
fawad irfan sheikh <fawad313@acm.org>
2023-11-28 00:24:39 +0000

    Initial Create of CommandAPI project

---
+++ CommandAPISolution/src/CommandAPI/Controllers/CommandsController.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CommandAPI.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class CommandsController : ControllerBase
+{
+    [HttpGet]
+    public ActionResult<IEnumerable<string>> Get()
+    {
+        return new string[] {"this","is","ard","coded"};
+    }
+}
